666d4aec10bd95479ef383ef33ce1ce4c68aa13e
walopes <walopes23@gmail.com>
2024-11-05 08:00:56 -0300

    Added pageable on listing

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,19 +1,5 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
@@ -28,7 +14,8 @@ public class MedicoController {
     }
 
     @GetMapping("listar")
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable p){
+        System.out.println(p.toString());
+        return repository.findAll(p).map(DadosListagemMedico::new);
     }
 }
