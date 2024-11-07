5f0374c203e737f4d4a4c427def5b98bc5491044
walopes <walopes23@gmail.com>
2024-11-07 10:55:24 -0300

    Added more exceptions handlers at TratadorDeErros

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,17 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var response = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(response.stream().map(DadosErrosValidacao::new));
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
