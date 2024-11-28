befbb852dd3eb996e7a4aa417d95efeafce2d1fb
walopes <walopes23@gmail.com>
2024-11-28 11:55:13 -0300

    Implemented DadosDetalhamentoPaciente record

--- src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -1,4 +1,13 @@
 package med.voll.api.paciente;
 
-public record DadosDetalhamentoPaciente() {
+public record DadosDetalhamentoPaciente(Long id, String nome, String email) {
+	
+	public DadosDetalhamentoPaciente(Paciente paciente){
+		this(
+			paciente.getId(),
+			paciente.getNome(),
+			paciente.getEmail()
+		);
+		
+	}
 }
