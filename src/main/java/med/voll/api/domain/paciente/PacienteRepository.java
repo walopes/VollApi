568815619e923b75a9405b1a97792a38f04da810
walopes <walopes23@gmail.com>
2024-12-10 10:48:13 -0300

    Method excluir

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -3,4 +3,7 @@ package med.voll.api.domain.paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+	
+	public excluir(){
+	}
 }
