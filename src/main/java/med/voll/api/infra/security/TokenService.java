2e4fadd5f9944971774e9add766db167fbf3b566
walopes <walopes23@gmail.com>
2024-12-27 16:51:58 -0300

    Implemented method GerarToken at TokenService

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,4 +1,26 @@
 package med.voll.api.infra.security;
 
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+@Service
 public class TokenService {
+    
+
+    public String GerarToken(){
+        try {
+            var algorithm = Algorithm.HMAC256("senha123");
+            return JWT.create()
+                .withIssuer("Api Voll.med")
+                .sign(algorithm);
+
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("error when generating JWT", exception);
+        }
+    }
+
 }
