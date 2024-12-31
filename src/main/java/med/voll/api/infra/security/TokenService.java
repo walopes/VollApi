fe2c6e97b79d136293591b3806d5880bf875bc74
walopes <walopes23@gmail.com>
2024-12-31 12:07:33 -0300

    Added claims and subject when generating Token

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -6,15 +6,19 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
+import med.voll.api.domain.user.Usuario;
+
 @Service
 public class TokenService {
     
 
-    public String GerarToken(){
+    public String GerarToken(Usuario usuario){
         try {
             var algorithm = Algorithm.HMAC256("senha123");
             return JWT.create()
                 .withIssuer("Api Voll.med")
+                .withSubject(usuario.getLogin())
+                .withClaim("id", usuario.getId())
                 .sign(algorithm);
 
         } catch (JWTCreationException exception){
