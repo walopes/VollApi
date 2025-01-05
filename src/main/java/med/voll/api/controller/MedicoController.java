94efbdc0a9325b61cc333db146dbebf4326457de
walopes <walopes23@gmail.com>
2025-01-05 17:35:36 -0300

    Fixed imports on the file

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,27 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import med.voll.api.domain.medico.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.medico.DadosCadastroMedico;
+import med.voll.api.domain.medico.DadosDetalhamentoMedico;
+import med.voll.api.domain.medico.DadosListagemMedico;
+import med.voll.api.domain.medico.DadosUpdateMedico;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
@@ -33,6 +46,8 @@ public class MedicoController {
     @GetMapping("/{id}")
     public ResponseEntity<DadosDetalhamentoMedico> get(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
+        if(medico == null)
+            return ResponseEntity.noContent().build();
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
