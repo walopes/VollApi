6c84a93a8db5e86b3373356b76ab6dd405ba3747
Willian Lopes <willian.lopes@dataprev.gov.br>
2025-01-09 09:22:28 -0300

    Implemented validation of token at SecurityFilter

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +17,17 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
         filterChain.doFilter(request, response);
     }
 
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null)
+            throw new RuntimeException("Token não enviado");
+
+        return authorizationHeader;
+    }
 }
