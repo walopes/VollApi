d4cdb1f0ab3abe3c5c564da6d452cc319ee5b3e3
Willian Lopes <walopes23@gmail.com>
2025-01-15 21:23:35 -0300

    Fixed Bearer from request by using trim()

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -42,7 +42,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null)
-            return authorizationHeader.replace("Bearer", "");
+            return authorizationHeader.replace("Bearer", "").trim();
         return null;
     }
 }
