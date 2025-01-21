a8d19b92a91a3c1937e909547e071b2d9c64a131
Willian Lopes <walopes23@gmail.com>
2025-01-21 08:01:25 -0300

    Added AgendaDeConsultas class

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,11 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    public void Agendar(DadosAgendamentoConsulta dados) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
