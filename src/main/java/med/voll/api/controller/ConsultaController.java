0f5de84e94bd0c721f02b3f5709c79b495782033
Willian Lopes <walopes23@gmail.com>
2025-01-22 13:41:53 -0300

    Injecting AgendaDeConsultas at ConsultaController

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agendaDeConsultas;
+
     @PostMapping
     @Transactional
     public ResponseEntity postMethodName(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        System.out.println(dados);
         // TODO: process POST request
 
         // return entity;
