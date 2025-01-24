d9c78f1ac0a8df549c2f63021749d0b6a83e90ee
Willian Lopes <walopes23@gmail.com>
2025-01-24 10:00:32 -0300

    Added logic to AgendadDeConsultas

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,30 @@
 package med.voll.api.domain.consulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
 
 @Service
 public class AgendaDeConsultas {
 
-    public void Agendar(DadosAgendamentoConsulta dados) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    @Autowired
+    private ConsultaRepository repository;
+
+    @Autowired
+    private MedicoRepository medicoRepo;
+
+    @Autowired
+    private PacienteRepository pacienteRepo;
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+
+        // TODO Add the business logic
+
+        var medico = medicoRepo.findById(dados.idMedico()).get();
+        var paciente = pacienteRepo.findById(dados.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.date());
+        repository.save(consulta);
     }
 }
