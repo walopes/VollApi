855a17139d636821dfa5c8fe872360a0a7a6c64f
Willian Lopes <walopes23@gmail.com>
2025-02-06 08:57:50 -0300

    Fixed V7 migration sql

--- src/main/resources/db/migration/V7__addConsultaEntity.sql
+++ src/main/resources/db/migration/V7__addConsultaEntity.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Consultas(
+CREATE TABLE consultas(
     id bigint not null auto_increment,
     medico_id bigint not null,
-    paciente_id bigint not_null,
-    data datetime not null
+    paciente_id bigint not null,
+    data datetime not null,
 
     primary key (id),
     constraint fk_consultas_medico_id foreign key(medico_id) references medicos(id),
