eda2712d12f5fd34eb29717e92bd07cd1b9d99f8
Willian Lopes <walopes23@gmail.com>
2025-02-10 09:20:11 -0300

    Using variable on Consulta

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -11,24 +11,26 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findAllByAtivoTrue(Pageable p);
 
+    public final Integer HOURS_TO_CANCEL = 24;
+
     @Query("""
             select m from Medico m
-                where m.ativo = true
-                and m.especialidade = :especialidade
-                and m.id not in (
-                    select c.medico.id from Consulta c
-                    where c.data = :data
-                )
-                order by rand()
-                limit 1;
+            where m.ativo = true
+            and m.especialidade = :especialidade
+            and m.id not in (
+            select c.medico.id from Consulta c
+            where c.data = :data
+            )
+            order by rand()
+            limit 1
             """)
     Medico findRandomMedico(Especialidade especialidade, LocalDateTime data);
 
     @Query("""
             SELECT con.id IS NOT NULL
-                FROM Consulta con
-                WHERE con.id = :idConsulta
-                AND (TIMESTAMPDIFF(HOUR, :data, con.data) >= 24);
-                """)
+            FROM Consulta con
+            WHERE con.id = :idConsulta
+            AND (TIMESTAMPDIFF(HOUR, :data, con.data) >= :HOURS_TO_CANCEL)
+            """)
     Boolean isAllowedToCancelAppointment(Long idConsulta, LocalDateTime data);
 }
