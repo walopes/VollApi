ddac6f2690289083ed6cdfca4382be243d816656
Willian Lopes <walopes23@gmail.com>
2025-02-11 10:05:11 -0300

    Added Date validations on ValidacaoHorario

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorario.java
@@ -1,5 +1,15 @@
 package med.voll.api.domain.consulta.validacoes;
 
+import java.time.DayOfWeek;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
 public class ValidacaoHorario {
 
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.date();
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAbertura = dataConsulta.getHour() < 7;
+        var depoisEncerramento = dataConsulta.getHour() > 18; // Each appointment have 1h - 19pm is the closing time
+    }
 }
