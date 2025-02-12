940dd55cf852dcf460d00f5bbf3383053ec7ed27
Willian Lopes <walopes23@gmail.com>
2025-02-12 09:49:39 -0300

    Included information

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorario.java
@@ -9,7 +9,8 @@ public class ValidacaoHorario {
     public void validar(DadosAgendamentoConsulta dados) {
         var dataConsulta = dados.date();
         var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
-        var antesDaAbertura = dataConsulta.getHour() < 7;
-        var depoisEncerramento = dataConsulta.getHour() > 18; // Each appointment have 1h - 19pm is the closing time
+        var antesDaAbertura = dataConsulta.getHour() < 7; 
+	// TODO Each appointment have 1h - 19pm is the closing time
+        var depoisEncerramento = dataConsulta.getHour() > 18;
     }
 }
