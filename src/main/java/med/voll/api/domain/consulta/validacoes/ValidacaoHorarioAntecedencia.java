6a151731911073794e4aa64070a1404070b4b237
Willian Lopes <walopes23@gmail.com>
2025-02-13 08:11:45 -0300

    Implemented method validar on ValidacaoHorarioAntecendecia

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
@@ -1,5 +1,16 @@
 package med.voll.api.domain.consulta.validacoes;
 
 public class ValidadorHorarioAntecedencia{
+    
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+	var agora = localDateTime.now();
+	var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+	if(diferencaEmMinutos < 30)
+	    throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+    }
+
+
 }
 
