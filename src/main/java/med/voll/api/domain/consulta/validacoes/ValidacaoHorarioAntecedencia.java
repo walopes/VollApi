2979f7c501e0dbb855e021a7b0eb21de21785631
Willian Lopes <walopes23@gmail.com>
2025-02-14 10:56:23 -0300

    Inserting in variable error message

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.consulta.validacoes;
 
 public class ValidadorHorarioAntecedencia{
+
+    private final String errorMessage = "Consulta deve ser agendada com antecedência mínima de 30 minutos";
     
     public void validar(DadosAgendamentoConsulta dados){
         var dataConsulta = dados.data();
@@ -8,7 +10,7 @@ public class ValidadorHorarioAntecedencia{
 	var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
 
 	if(diferencaEmMinutos < 30)
-	    throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+	    throw new ValidacaoException(this.errorMessage);
     }
 
 
