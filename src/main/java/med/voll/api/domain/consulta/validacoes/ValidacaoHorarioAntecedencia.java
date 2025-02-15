e9d1c8e7bebaa3e65c9cbc087351f4c305bdce4f
Willian Lopes <walopes23@gmail.com>
2025-02-15 20:49:07 -0300

    Variables used in class

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
@@ -2,15 +2,16 @@ package med.voll.api.domain.consulta.validacoes;
 
 public class ValidadorHorarioAntecedencia{
 
-    private final String errorMessage = "Consulta deve ser agendada com antecedência mínima de 30 minutos";
+    private final Int DURATION_TIME = 30;
+    private final String ERROR_MESSAGE = "Consulta deve ser agendada com antecedência mínima de 30 minutos";
     
     public void validar(DadosAgendamentoConsulta dados){
         var dataConsulta = dados.data();
 	var agora = localDateTime.now();
 	var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
 
-	if(diferencaEmMinutos < 30)
-	    throw new ValidacaoException(this.errorMessage);
+	if(diferencaEmMinutos < DURATION_TIME)
+	    throw new ValidacaoException(this.ERROR_MESSAGE);
     }
 
 
