f52f28a570e679c08495129140be3ebf565d62f1
Willian Lopes <walopes23@gmail.com>
2025-02-18 10:07:44 -0300

    Fixed file

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
@@ -1,18 +1,4 @@
 package med.voll.api.domain.consulta.validacoes;
 
 public class ValidacaoMedicoAtivo{
-    
-    private MedicoRepository repository;
-
-    private final String ERROR_MESSAGE = "Consulta não pode ser agendada com médico excluído";
-
-    public void validar(DadosAgendamentoConsulta dados){
-        if(dados.idMedico() == null)
-		return;
-
-	var isMedicoAtivo = repository.findAtivoById(dados.idMedico());
-	if(!isMedicoAtivo)
-	    throw new ValidacaoException(ERROR_MESSAGE);
-
-    }
 }
