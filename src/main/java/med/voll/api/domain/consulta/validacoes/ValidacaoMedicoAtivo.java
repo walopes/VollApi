b9014d14134da678b883c47be03cc14c16c4ac29
Willian Lopes <walopes23@gmail.com>
2025-02-18 10:08:58 -0300

    Added method validar on ValidacaoMedicoAtivo

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
@@ -1,4 +1,10 @@
 package med.voll.api.domain.consulta.validacoes;
 
 public class ValidacaoMedicoAtivo{
+    
+    @AutoWire
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+    }
 }
