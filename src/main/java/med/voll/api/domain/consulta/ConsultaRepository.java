912d91fa4a35dfe45f7cb49ca6b086782de80bf7
Willian Lopes <walopes23@gmail.com>
2025-02-23 22:22:20 -0300

    Added new methods to ConsultaRepository

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,4 +3,8 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+	boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+
+	boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
