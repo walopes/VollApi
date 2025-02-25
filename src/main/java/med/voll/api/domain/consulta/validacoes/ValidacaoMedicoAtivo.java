0f39f695c75451d2f0270ab9da987067caaaa5f8
Willian Lopes <walopes23@gmail.com>
2025-02-25 11:04:56 -0300

    Passing error message as string

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoAtivo.java
@@ -5,13 +5,15 @@ public class ValidacaoMedicoAtivo{
     @AutoWire
     private MedicoRepository repository;
 
+    private final String ERROR_MESSAGE = "Consulta não pode ser agendada com médico excluído";
+
     public void validar(DadosAgendamentoConsulta dados){
         if(dados.idMedico() == null)
 		return;
 
 	var isMedicoAtivo = repository.findAtivoById(dados.idMedico());
 	if(!isMedicoAtivo)
-	    throw new ValidacaoException("Consulta não pode ser agendada com médico excluído");
+	    throw new ValidacaoException(ERROR_MESSAGE);
 
     }
 }
