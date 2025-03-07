aea02beffbdde40bc0a004c764a040c0a7d6eae8
Willian Lopes <walopes23@gmail.com>
2025-03-07 17:35:15 -0300

    Added new features to ValidacaoMedicoComConsultaMesmoHorario

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoComConsultaMesmoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoComConsultaMesmoHorario.java
@@ -1,5 +1,13 @@
 package med.voll.api.domain.consulta.validacoes;
 
 
-public class ValidacaoMedicoComOutraConsultaMesmoHorario{
+public class ValidacaoMedicoComConsultaMesmoHorario{
+	
+	private ConsultaRepository repository;
+
+	public  void validar(DadosAgendamentoConsulta dados){
+	    var medicoPossuiConsultaHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+	    if(medicoPossuiConsultaHorario)
+		    throw new ValidacaoException("Médico já possui outra consulta agendada nesse mesmo horário!");
+	}
 }
