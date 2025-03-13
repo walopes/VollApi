305a3bda2f401c3e461d677825210d8dc4946504
Willian Lopes <walopes23@gmail.com>
2025-03-13 14:17:59 -0300

    Renamed File

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoComConsultaMesmoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoComConsultaMesmoHorario.java
@@ -2,12 +2,13 @@ package med.voll.api.domain.consulta.validacoes;
 
 
 public class ValidacaoMedicoComConsultaMesmoHorario{
-	
-	private ConsultaRepository repository;
 
-	public  void validar(DadosAgendamentoConsulta dados){
-	    var medicoPossuiConsultaHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
-	    if(medicoPossuiConsultaHorario)
-		    throw new ValidacaoException("Médico já possui outra consulta agendada nesse mesmo horário!");
-	}
+    private final String ERROR_MESSAGE = "Médico já possui outra consulta agendada nesse mesmo horário!";
+    private ConsultaRepository repository;
+
+    public  void validar(DadosAgendamentoConsulta dados){
+        var medicoPossuiConsultaHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if(medicoPossuiConsultaHorario)
+	    throw new ValidacaoException(ERROR_MESSAGE);
+    }
 }
