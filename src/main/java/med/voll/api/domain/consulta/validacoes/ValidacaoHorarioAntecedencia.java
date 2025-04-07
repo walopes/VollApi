f97527252a9785aea2af60cac826f4e14d991305
Willian Lopes <walopes23@gmail.com>
2025-04-07 09:52:59 -0300

    Fixed some validation methods

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioAntecedencia.java
@@ -1,19 +1,23 @@
 package med.voll.api.domain.consulta.validacoes;
 
-public class ValidadorHorarioAntecedencia{
+import java.time.Duration;
+import java.time.LocalDateTime;
 
-    private final Int DURATION_TIME = 30;
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidacaoHorarioAntecedencia {
+
+    private final int DURATION_TIME = 30;
     private final String ERROR_MESSAGE = "Consulta deve ser agendada com antecedência mínima de 30 minutos";
-    
-    public void validar(DadosAgendamentoConsulta dados){
-        var dataConsulta = dados.data();
-	var agora = localDateTime.now();
-	var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
 
-	if(diferencaEmMinutos < DURATION_TIME)
-	    throw new ValidacaoException(this.ERROR_MESSAGE);
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < DURATION_TIME)
+            throw new ValidacaoException(this.ERROR_MESSAGE);
     }
 
-
 }
-
--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioFuncionamento.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioFuncionamento.java
@@ -2,18 +2,19 @@ package med.voll.api.domain.consulta.validacoes;
 
 import java.time.DayOfWeek;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 
 public class ValidacaoHorarioFuncionamento {
 
     public void validar(DadosAgendamentoConsulta dados) {
-        var dataConsulta = dados.date();
+        var dataConsulta = dados.data();
         var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
-        var antesDaAbertura = dataConsulta.getHour() < 7; 
-	// TODO Each appointment have 1h - 19pm is the closing time
+        var antesDaAbertura = dataConsulta.getHour() < 7;
+        // TODO Each appointment have 1h - 19pm is the closing time
         var depoisEncerramento = dataConsulta.getHour() > 18;
 
-	if(domingo || antesDaAbertura || depoisEncerramento)
-	    throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica");
+        if (domingo || antesDaAbertura || depoisEncerramento)
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica");
     }
 }
