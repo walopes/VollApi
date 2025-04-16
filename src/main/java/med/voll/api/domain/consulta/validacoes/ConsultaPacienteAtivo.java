75eeaa077f4925928a98ecbe5d98b4e46165fcfe
Willian Lopes <walopes23@gmail.com>
2025-04-16 14:47:15 -0300

    Added new implementations at ConsultaPacienteAtivo

--- src/main/java/med/voll/api/domain/consulta/validacoes/ConsultaPacienteAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ConsultaPacienteAtivo.java
@@ -1,21 +1,17 @@
 package med.voll.api.domain.consulta.validacoes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.paciente.DadosPaciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+
 public class ConsultaPacienteAtivo {
 
-    // TODO to implement
+    @Autowired
+    private PacienteRepository repository;
 
-    // private MedicoRepository repository;
-
-    // private final String ERROR_MESSAGE = "Consulta não pode ser agendada com
-    // médico excluído";
-
-    // public void validar(DadosAgendamentoConsulta dados) {
-    // if (dados.idMedico() == null)
-    // return;
-
-    // var isMedicoAtivo = repository.findAtivoById(dados.idMedico());
-    // if (!isMedicoAtivo)
-    // throw new ValidacaoException(ERROR_MESSAGE);
-
-    // }
+    public void validar(DadosPaciente paciente) {
+        // TODO
+    }
 }
