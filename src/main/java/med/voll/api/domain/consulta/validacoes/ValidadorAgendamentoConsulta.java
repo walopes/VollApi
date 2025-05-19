96a52fb5bff4d3aa9480a3e2751f9827f02e72ae
Willian Lopes <walopes23@gmail.com>
2025-05-19 14:05:24 -0300

    Added component

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoConsulta.java
@@ -2,6 +2,7 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 
+@Component
 public interface ValidadorAgendamentoConsulta {
     void validar(DadosAgendamentoConsulta dados);
 }
