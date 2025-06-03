1d515273582f20e5df134f89a46dda183b14957a
Willian A. Lopes <walopes23@gmail.com>
2025-06-03 13:54:43 -0300

    Fix database for testing

--- src/helper/database.sql
+++ src/helper/database.sql
@@ -2,4 +2,4 @@
 INSERT INTO  usuarios(id, login, senha) VALUES (1, 'john.doe@voll.med', ' $2a$12$jCoT.TJzT2/6lzCErgfmuuW3ZeONEcj3kjrYYwCEKHAwMfeK5usgS');
 
 -- new database for testing purpose
-CREATE DATABASE vollmed_api_test;
+CREATE DATABASE voll_api_test;
