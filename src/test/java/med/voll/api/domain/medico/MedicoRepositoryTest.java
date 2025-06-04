6adcb6a3a09c5f474b22f3260d3cd33e22738d1b
Willian Lopes <walopes23@gmail.com>
2025-06-04 18:48:04 -0300

    Inserting data with SpringDataJPA on the test database for MedicoRepository

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,18 +1,83 @@
 package med.voll.api.domain.medico;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+
+import med.voll.api.domain.endereco.DadosEndereco;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test") // load database from 'application-test.properties'
 public class MedicoRepositoryTest {
 
-    @Test
-    public void escolherMedicoAleatorioLivreNaData() {
+    @Autowired
+    private MedicoRepository medicoRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    @DisplayName("Deveria devolver null quando único médico cadastrado não está disponível na data")
+    public void escolherMedicoAleatorioLivreNaDataCenario1() {
+
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+
+        var medico = cadastroMedico("Medico", "medico@voll.med", "123456", Especialidade.CARDIOLOGIA);
+        // TODO finalizar métodos paciente para prosseguir
+        // var paciente = cadastrarPaciente("Paciente", "paciente@email.com",
+        // "00000000000");
+        // var consulta = cadastrarConsulta(medico, paciente, proximaSegundaAs10);
+        // cadastrarConsulta(medico, paciente, proximaSegundaAs10);
+
+        var medicoLivre = medicoRepository.findRandomMedico(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+        assertThat(medicoLivre).isNull();
+    }
+
+    private Medico cadastroMedico(String nome, String email, String crm, Especialidade especialidade) {
+        var medico = new Medico(dadosMedico(nome, email, crm, especialidade));
+        em.persist(medico);
+        return medico;
+    }
+
+    private DadosCadastroMedico dadosMedico(String nome, String email, String crm, Especialidade especialidade) {
+        return new DadosCadastroMedico(
+                nome,
+                email,
+                "123456",
+                crm,
+                especialidade,
+                dadosEndereco());
+    }
+
+    // private DadosCadastroPaciente dadosPaciente(String nome, String email, String
+    // cpf){
+    // return new DadosCadastroPaciente(
+
+    // );
+    // }
+
+    private DadosEndereco dadosEndereco() {
+        return new DadosEndereco(
+                "rua xpto",
+                "bairro",
+                "0000000",
+                "Brasilia",
+                "DF",
+                null,
+                null);
     }
 
 }
