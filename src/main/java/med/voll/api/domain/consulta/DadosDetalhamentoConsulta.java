aede4c4afbe79cdc6d05906bc86668ec0a92f925
Willian Lopes <walopes23@gmail.com>
2025-06-04 18:49:15 -0300

    Changed return type of AgendaDeConsultas' agendar method to DadosDetalhamentoConsulta

--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -3,9 +3,16 @@ package med.voll.api.domain.consulta;
 import java.time.LocalDateTime;
 
 public record DadosDetalhamentoConsulta(
-        Long id,
-        Long idMedico,
-        Long idPaciente,
-        LocalDateTime data) {
+                Long id,
+                Long idMedico,
+                Long idPaciente,
+                LocalDateTime data) {
+
+        public DadosDetalhamentoConsulta(Consulta consulta) {
+                this.id = consulta.getId();
+                this.idMedico = consulta.getMedico().getId();
+                this.idPaciente = consulta.getPaciente().getId();
+                this.data = consulta.getData();
+        }
 
 }
