dbdef398130bd05a39994e2492e1ff281bea1d52
Willian Lopes <walopes23@gmail.com>
2025-06-13 13:47:43 -0300

    Renamed DadosPaciente to DadosCadastroPaciente

--- src/main/java/med/voll/api/domain/paciente/DadosPaciente.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.domain.paciente;
-
-import jakarta.validation.constraints.NotNull;
-import med.voll.api.domain.endereco.DadosEndereco;
-
-public record DadosPaciente(@NotNull Long id, String nome, String email, DadosEndereco endereco) {
-}
