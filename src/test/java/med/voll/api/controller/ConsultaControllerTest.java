99fa482998b9bdc5ef15786fb7663979a31aba14
Willian Lopes <walopes23@gmail.com>
2025-06-24 11:05:58 -0300

    Added new unitary test for verifying the response of the request at ConsultaControllerTest

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -2,20 +2,39 @@ package med.voll.api.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.time.LocalDateTime;
+
 import static org.springframework.security.test.context.support.WithMockUser;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.medico.Especialidade;
+
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @AutoWired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+
+    @AutoWired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
 
     @Test
     @DisplayName("Deveria devolver código HTTP 400 quando informações estão inválidas")
@@ -28,10 +47,25 @@ public class ConsultaControllerTest {
     }
 
     @Test
-    @DisplayName("TODO")
+    @DisplayName("Deveria devolver código HTTP 200 quando informações estão válidas")
     void testAgendarCenario2() {
-        // TODO
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var response = mvc.perform(post("/consultas").contentType(MediaType.APPLICATION_JSON)
+                .content(dadosAgendamentoConsultaJson.write(
+                        new DadosAgendamentoConsulta(2l, 5l, data, especialidade)).getJson()))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsultaJson.write(
+                new DadosDetalhamentoConsulta(null, 2l, 5l, data)).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
+
+    // TODO Do the unitary test
 
     // @Test
     // void testCancelar() {
