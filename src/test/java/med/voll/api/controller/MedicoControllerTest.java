7fd617e2da3dd50bcf9a46751121b6eb1b5563cb
Willian Lopes <walopes23@gmail.com>
2025-06-30 09:51:06 -0300

    Added MedicoControllerTest (missing implementation though)

---
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+// TODO Implement unitary tests for method
+public class MedicoControllerTest {
+    @Test
+    void testCadastrar() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testGet() {
+
+    }
+
+    @Test
+    void testListar() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
