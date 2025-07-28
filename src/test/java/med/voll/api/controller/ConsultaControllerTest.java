defe4941335b51698447036e9ca0f99654558295
Willian Lopes <walopes23@gmail.com>
2025-07-27 22:42:22 -0300

    Re-done

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.time.LocalDateTime;
 
@@ -30,53 +30,53 @@ import med.voll.api.domain.medico.Especialidade;
 @AutoConfigureJsonTesters
 public class ConsultaControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+        @Autowired
+        private MockMvc mvc;
 
-    @Autowired
-    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+        @Autowired
+        private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
 
-    @Autowired
-    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
+        @Autowired
+        private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
 
-    @MockBean // Tell spring to mock this class
-    private AgendaDeConsultas agendaDeConsultas;
+        @MockBean // Tell spring to mock this class
+        private AgendaDeConsultas agendaDeConsultas;
 
-    @Test
-    @DisplayName("Deveria devolver código HTTP 400 quando informações estão inválidas")
-    @WithMockUser
-    void testAgendarCenario1() throws Exception {
-        var response = mvc.perform(post("/consultas"))
-                .andReturn().getResponse();
+        @Test
+        @DisplayName("Deveria devolver código HTTP 400 quando informações estão inválidas")
+        @WithMockUser
+        void testAgendarCenario1() throws Exception {
+                var response = mvc.perform(post("/consultas"))
+                                .andReturn().getResponse();
 
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
+                assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
 
-    @Test
-    @DisplayName("Deveria devolver código HTTP 200 quando informações estão válidas")
-    void testAgendarCenario2() throws Exception {
-        var data = LocalDateTime.now().plusHours(1);
-        var especialidade = Especialidade.CARDIOLOGIA;
-        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2l, 5l, data);
-        when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
+        @Test
+        @DisplayName("Deveria devolver código HTTP 200 quando informações estão válidas")
+        void testAgendarCenario2() throws Exception {
+                var data = LocalDateTime.now().plusHours(1);
+                var especialidade = Especialidade.CARDIOLOGIA;
+                var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2l, 5l, data);
+                when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
 
-        var response = mvc.perform(post("/consultas").contentType(MediaType.APPLICATION_JSON)
-                .content(dadosAgendamentoConsultaJson.write(
-                        new DadosAgendamentoConsulta(2l, 5l, data, especialidade)).getJson()))
-                .andReturn().getResponse();
+                var response = mvc.perform(post("/consultas").contentType(MediaType.APPLICATION_JSON)
+                                .content(dadosAgendamentoConsultaJson.write(
+                                                new DadosAgendamentoConsulta(2l, 5l, data, especialidade)).getJson()))
+                                .andReturn().getResponse();
 
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+                assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 
-        var jsonEsperado = dadosDetalhamentoConsultaJson.write(
-                new DadosDetalhamentoConsulta(null, 2l, 5l, data)).getJson();
+                var jsonEsperado = dadosDetalhamentoConsultaJson.write(
+                                new DadosDetalhamentoConsulta(null, 2l, 5l, data)).getJson();
 
-        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
-    }
+                assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+        }
 
-    // TODO Do the unitary test
+        // TODO Do the unitary test
 
-    // @Test
-    // void testCancelar() {
+        // @Test
+        // void testCancelar() {
 
-    // }
+        // }
 }
